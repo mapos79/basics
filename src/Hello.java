0fa1c47dba2221b643c48f49dc0b290ea75673dc
mapos79 <matepos79@gmail.com>
2018-09-04 22:04:08 +0200

    integer plays

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,16 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi, Matej!");
+        System.out.println("Hello Tim!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
